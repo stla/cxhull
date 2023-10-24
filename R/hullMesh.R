97bd64bd8e66d354dcab79a631cc78c1f3656e49
stla <laurent_step@yahoo.fr>
2023-10-24 04:46:24 +0200

    v0.7.4

--- R/hullMesh.R
+++ R/hullMesh.R
@@ -48,6 +48,9 @@ orderFace <- function(face) {
 #' library(rgl)
 #' open3d(windowRect = c(50, 50, 562, 562))
 #' shade3d(septuaginta, color = "darkred")
+#' # some quad faces are misoriented:
+#' open3d(windowRect = c(50, 50, 562, 562))
+#' shade3d(septuaginta, color = "tomato", back = "culled")
 hullMesh <- function(hull, simplify = TRUE, rgl = FALSE) {
   stopifnot(isBoolean(simplify))
   stopifnot(isBoolean(rgl))
